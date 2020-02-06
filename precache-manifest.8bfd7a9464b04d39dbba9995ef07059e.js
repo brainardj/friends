0ef576ac03ddc260072e7885823970c5e9a8f20b
jack <brainard@my.lanecc.edu>
2020-02-05 17:10:41 -0800

    Updates

---
+++ precache-manifest.8bfd7a9464b04d39dbba9995ef07059e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "595feff3b545dba5387644c00688a634",
+    "url": "/friends/index.html"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/friends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "dcbd6600c03575272c6d",
+    "url": "/friends/static/css/main.ca54f406.chunk.css"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/friends/static/js/2.2a21f297.chunk.js"
+  },
+  {
+    "revision": "dcbd6600c03575272c6d",
+    "url": "/friends/static/js/main.170e86c9.chunk.js"
+  },
+  {
+    "revision": "acf25158001195f2b7f8",
+    "url": "/friends/static/js/runtime-main.c63e69b2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
